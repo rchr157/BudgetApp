7c39b40756576ecacf937d4ac25a119b03566cc2
christian <r.chris47@outlook.com>
2020-01-12 18:16:33 -0800

    Converted all Budgetapp to "self.controller" object. Fixed filter bug that did not want to show categories checked and messed with plotting. Next: Continue TODOs and include additional features

--- GUI.py
+++ GUI.py
@@ -40,28 +40,7 @@ def update_menu(sel_menu, sel_opt):
                          command=lambda value=string: sel_menu.set(value))
 
 
-def okayfun(top):
-    # update Price dictionary
-    if not top.max_entry.get() == "":
-        Budgetapp.price_dict["max"] = float(top.max_entry.get())
-    if not top.min_entry.get() == "":
-        Budgetapp.price_dict["min"] = float(top.min_entry.get())
 
-    # # Verify Code is working
-    # print("Max Price: " + str(Budgetapp.price_dict.loc[0, 'max']))
-    # print("Min Price: " + str(Budgetapp.price_dict.loc[0, 'min']))
-    # print(top.max_entry.get())
-    # print(top.min_entry.get())
-
-    # TODO: update OptionMenu for Categories
-    # menu = BudgetPage.dropdown["menu"]
-    # menu.delete(0, "end")
-    # for string in BudgetPage.testdd:
-    #     menu.add_command(label=string,
-    #                      command=lambda value=string: BudgetPage.ddvar.set(value))
-    # BudgetPage.update_option_menu(BudgetPage)
-
-    top.destroy()
 
 
 def update_dict(key, dict):
@@ -76,91 +55,8 @@ def update_dict(key, dict):
         dict[key].set(1)
     # print(dict[key].get())
 
+# def popupfilters(self):
 
-# def popupbudget():
-#     top = tk.Tk()
-#     top.wm_title("Budget Summary")
-
-
-def popupfilters():
-    top = tk.Tk()
-    top.wm_title("Filter Options")
-
-    label = ttk.Label(top, text="Select which filters to apply", font=NORM_FONT)
-    # label.pack(side="top", fill="x", pady=10)
-    label.grid(row=0, column=0, padx=5, pady=5)
-
-    # Accounts Filter
-    accounts_label = tk.Label(top, text="Accounts: ", font=NORM_FONT)
-    accounts_label.grid(row=1, column=0, sticky="W")
-
-    # account_list = Budgetapp.dataframe["Account Name"].unique()
-    # account_list = [ac.lower() for ac in account_list]
-    # account_list.sort()
-    # Budgetapp.account_dict = dict()
-    # Budgetapp.account_dict = {acct1: tk.IntVar(value=1, name=acct1) for acct1 in account_list}
-    r = 0
-    row = 2
-    # print("Account Loop")
-    for acct in Budgetapp.account_dict.keys():
-        # print(acct)
-        # print(Budgetapp.account_dict[acct])
-        # print(Budgetapp.account_dict[acct].get())
-        chk = tk.Checkbutton(top, text=acct, variable=Budgetapp.account_dict[acct], onvalue=1, offvalue=0,
-                             command=lambda account_name=acct: update_dict(key=account_name,
-                                                                           dict=Budgetapp.account_dict))
-        # chk.select()
-        if Budgetapp.account_dict[acct].get(): chk.select()
-        chk.grid(row=row, column=r, pady=5, sticky="W")
-        r += 1
-        if r > 3:
-            r = 0
-            row = 3
-
-    # Category Filters
-    category_label = tk.Label(top, text="Categories: ", font=NORM_FONT)
-    category_label.grid(row=row + 1, column=0, sticky="W")
-
-    # category_list = Budgetapp.dataframe["Category2"].unique()
-    # category_list.sort()
-    # Budgetapp.category_dict = dict()
-    # Budgetapp.category_dict = {cat: tk.IntVar(value=1, name=cat) for cat in category_list}
-    r = 0
-    row += 2
-    for cat1 in Budgetapp.category_dict.keys():
-        chk = tk.Checkbutton(top, text=cat1, variable=Budgetapp.category_dict[cat1], onvalue=1, offvalue=0,
-                             command=lambda category=cat1: update_dict(key=category, dict=Budgetapp.category_dict))
-
-        if Budgetapp.category_dict[cat1].get(): chk.select()
-
-        chk.grid(row=row, column=r, pady=5, sticky="W")
-        r += 1
-        if r > 3:
-            r = 0
-            row += 1
-
-    # Price Range Filter
-    price_label = tk.Label(top, text="Price Range: ", font=NORM_FONT)
-    price_label.grid(row=row + 1, column=0, sticky="W")
-
-    min_label = tk.Label(top, text="Min")
-    min_label.grid(row=row + 2, column=0, pady=5)
-
-    top.min_entry = tk.Entry(top, text=str(Budgetapp.price_dict.loc[0, 'min']))
-    top.min_entry.grid(row=row + 2, column=1, pady=5, sticky="W")
-
-    max_label = tk.Label(top, text="Max")
-    max_label.grid(row=row + 2, column=2, pady=5)
-
-    top.max_entry = tk.Entry(top, text=str(Budgetapp.price_dict.loc[0, 'max']))
-    top.max_entry.grid(row=row + 2, column=4, pady=5, sticky="W")
-
-    # cancelbtn = ttk.Button(top, text="Cancel", command=top.destroy)
-    # # cancelbtn.pack()
-    # cancelbtn.grid(row=row+3, column=3, padx=5, pady=5, sticky="W")
-
-    okaybutton = ttk.Button(top, text="Okay", command=lambda: okayfun(top))
-    okaybutton.grid(row=row + 3, column=3, padx=5, pady=5, sticky="W")
 
 
 # def animate(i):
@@ -180,18 +76,14 @@ class Budgetapp(tk.Tk):
         self.filename = ""
         self.dataframe = pd.DataFrame()
         self.account_dict = dict()
-        # self.category_dict = dict()
-        # self.category_dict = {cat: tk.IntVar(value=1, name=cat) for cat in category_list}
-        # self.budget_dict = dict()
-        self.budget_dict = {cat2: tk.DoubleVar(value=0, name=cat2) for cat2 in category_list}
+        self.category_dict = {cat: tk.IntVar(value=1, name=cat) for cat in category_list}
+        self.budget_dict = {cat2: tk.DoubleVar(value=0, name=cat2+"-bgt") for cat2 in category_list}
         self.price_dict = pd.DataFrame()
 
         p1 = tk.Image('photo', file='wallet.png')
 
         # Setting icon of master window
-        # self.call('wm', 'iconphoto', self._w, p1)
-        # tk.Tk.iconphoto(self, default=p1) # Didn't work with png file DELETE
-        # tk.Tk.iconbitmap(self, default="wallet.bmp") # also didn't work DELETE
+        self.call('wm', 'iconphoto', self._w, p1)
         tk.Tk.wm_title(self, "Budget App")
 
         # Create container for window frames
@@ -273,8 +165,6 @@ class StartPage(tk.Frame):
         self.edit_browse = tk.Entry(self)
         self.edit_browse.grid(row=1, column=1)
 
-        # button = ttk.Button(self, text="Load",
-        #                     command=lambda: controller.show_frame(BudgetPage))
         button_load = ttk.Button(self, text="Load", command=self.load)
         button_load.grid(row=1, column=2)
 
@@ -305,71 +195,19 @@ class StartPage(tk.Frame):
             df = pd.Series()
             self.load_text.set("File type not supported")
 
-        Budgetapp.filename = filename
         df = df.applymap(lambda s: s.lower() if type(s) == str else s)  # apply lowercase to all columns
-        Budgetapp.dataframe = df
 
-        # Get max and min price for price dict
-        Budgetapp.price_dict = pd.DataFrame(data={"max": [df["Amount"].max()], "min": [df["Amount"].min()]})
-
-        account_list = Budgetapp.dataframe["Account Name"].unique()
+        # Get sorted list of Account names
+        account_list = df["Account Name"].unique()
         account_list = [ac.lower() for ac in account_list]
         account_list.sort()
-        Budgetapp.account_dict = dict()
-        Budgetapp.account_dict = {acct1: tk.IntVar(value=1, name=acct1) for acct1 in account_list}
 
-        #TODO: check if the code is not needed anymore
-        # category_list = df["Category2"].unique()
-        # category_list.sort()
-        # Budgetapp.category_dict = dict()
-        Budgetapp.category_dict = {cat3: tk.IntVar(value=1, name=cat3) for cat3 in category_list}
-        # Budgetapp.budget_dict = dict()
-        Budgetapp.budget_dict = {cat4: tk.DoubleVar(value=0.0, name=cat4) for cat4 in category_list}
-        # Budgetapp.budget_dict.update({"Savings": tk.DoubleVar(value=0.0, name="Savings")})
-        # years = Budgetapp.dataframe.sort_index().index.strftime("%Y").astype("int")
-        # opt_year = list(range(years[0], years[-1] + 1, 1))
-
-
-# Example page
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="This is Page One")
-        label.pack(pady=10, padx=10)
-
-        button1 = ttk.Button(self, text="Visit Start Page",
-                             command=lambda: controller.show_frame(StartPage))
-        button1.pack()
-
-        button2 = ttk.Button(self, text="Visit Page 2",
-                             command=lambda: controller.show_frame(BudgetPage))
-        button2.pack()
-
-        # button3 = ttk.Button(self, text="Visit Graph Page",
-        #                      command=lambda: controller.show_frame(PageThree))
-        # button3.pack()
-
-
-# original budget page
-# class BudgetPage(tk.Frame):
-#
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent)
-#         label = tk.Label(self, text="This Page is for Graphs!!!")
-#         label.pack(pady=10, padx=10)
-#
-#         button1 = ttk.Button(self, text="Visit Start Page",
-#                              command=lambda: controller.show_frame(StartPage))
-#         button1.pack()
-#
-#         canvas = FigureCanvasTkAgg(f, self)
-#         canvas.draw()
-#         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-#
-#         toolbar = NavigationToolbar2Tk(canvas, self)
-#         toolbar.update()
-#         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        # Save File name, Dataframe, Max/min Prices
+        self.controller.filename = filename
+        self.controller.dataframe = df
+        self.controller.price_dict = pd.DataFrame(data={"max": [df["Amount"].max()], "min": [df["Amount"].min()]})
+        # Create dictionary for widget use later on
+        self.controller.account_dict = {acct1: tk.IntVar(value=1, name=acct1) for acct1 in account_list}
 
 
 class BudgetPage(tk.Frame):
@@ -422,7 +260,7 @@ class BudgetPage(tk.Frame):
         self.endyear_dd.grid(row=2, column=3, sticky="WE")  # pack(side=tk.LEFT, padx=2)
 
         # Button to select which filters to apply
-        filterBtn = ttk.Button(self, text="Set Filters", command=popupfilters)
+        filterBtn = ttk.Button(self, text="Set Filters", command=lambda: self.popupfilters())
         filterBtn.grid(row=3, column=0, columnspan=2)  # pack(side=tk.RIGHT)
 
         # Drop Down menu to select which category to plot
@@ -445,10 +283,6 @@ class BudgetPage(tk.Frame):
         self.canvas.get_tk_widget().grid(row=4, column=0, columnspan=12, rowspan=16, padx=10,
                                     pady=10)  # pack(side=tk.BOTTOM, fill=tk.BOTH, padx=10, pady=10)  # fill=tk.BOTH, , expand=True
 
-        # toolbar = NavigationToolbar2Tk(canvas, self)
-        # toolbar.update()
-        # canvas.get_tk_widget().pack(side=tk.BOTTOM, expand=True)  # fill=tk.BOTH,
-
         # Button to calculate budget
         budget_button = ttk.Button(self, text="Calculate Budget",
                                    command=self.budget_cal)
@@ -460,8 +294,93 @@ class BudgetPage(tk.Frame):
             tk.Entry(self, textvariable=self.controller.budget_dict[cat5]).grid(row=r, column=15)
             r += 1
 
-        # tk.Label(self, text='Savings').grid(row=r, column=14)
-        # tk.Entry(self, textvariable=tk.StringVar()).grid(row=r, column=15)
+
+    def popupfilters(self):
+        top = tk.Tk()
+        top.wm_title("Filter Options")
+
+        label = ttk.Label(top, text="Select which filters to apply", font=NORM_FONT)
+        # label.pack(side="top", fill="x", pady=10)
+        label.grid(row=0, column=0, padx=5, pady=5)
+
+        # Accounts Filter
+        accounts_label = tk.Label(top, text="Accounts: ", font=NORM_FONT)
+        accounts_label.grid(row=1, column=0, sticky="W")
+
+        r = 0
+        row = 2
+        # print("Account Loop")
+        for acct in self.controller.account_dict.keys():
+            chk = tk.Checkbutton(top, text=acct, variable=self.controller.account_dict[acct], onvalue=1, offvalue=0,
+                                 command=lambda account_name=acct: update_dict(key=account_name,
+                                                                               dict=self.controller.account_dict))
+            # chk.select()
+            if self.controller.account_dict[acct].get(): chk.select()
+            chk.grid(row=row, column=r, pady=5, sticky="W")
+            r += 1
+            if r > 3:
+                r = 0
+                row = 3
+
+        # Category Filters
+        category_label = tk.Label(top, text="Categories: ", font=NORM_FONT)
+        category_label.grid(row=row + 1, column=0, sticky="W")
+
+        r = 0
+        row += 2
+        for cat1 in self.controller.category_dict.keys():
+            chk = tk.Checkbutton(top, text=cat1, variable=self.controller.category_dict[cat1], onvalue=1, offvalue=0,
+                                 command=lambda category=cat1: update_dict(key=category,
+                                                                           dict=self.controller.category_dict))
+
+            if self.controller.category_dict[cat1].get(): chk.select()
+            chk.grid(row=row, column=r, pady=5, sticky="W")
+            r += 1
+            if r > 3:
+                r = 0
+                row += 1
+
+        # Price Range Filter
+        price_label = tk.Label(top, text="Price Range: ", font=NORM_FONT)
+        price_label.grid(row=row + 1, column=0, sticky="W")
+
+        min_label = tk.Label(top, text="Min")
+        min_label.grid(row=row + 2, column=0, pady=5)
+
+        top.min_entry = tk.Entry(top, text=str(self.controller.price_dict.loc[0, 'min']))
+        top.min_entry.grid(row=row + 2, column=1, pady=5, sticky="W")
+
+        max_label = tk.Label(top, text="Max")
+        max_label.grid(row=row + 2, column=2, pady=5)
+
+        top.max_entry = tk.Entry(top, text=str(self.controller.price_dict.loc[0, 'max']))
+        top.max_entry.grid(row=row + 2, column=4, pady=5, sticky="W")
+
+        okaybutton = ttk.Button(top, text="Okay", command=lambda: okayfun(self.controller, top=top))  # lambda: okayfun(top)
+        okaybutton.grid(row=row + 3, column=3, padx=5, pady=5, sticky="W")
+
+        def okayfun(cont, top):
+            # update Price dictionary
+            if not top.max_entry.get() == "":
+                cont.price_dict["max"] = float(top.max_entry.get())
+            if not top.min_entry.get() == "":
+                cont.price_dict["min"] = float(top.min_entry.get())
+
+            # # Verify Code is working
+            # print("Max Price: " + str(cont.price_dict.loc[0, 'max']))
+            # print("Min Price: " + str(cont.price_dict.loc[0, 'min']))
+            # print(top.max_entry.get())
+            # print(top.min_entry.get())
+
+            #TODO: update OptionMenu for Categories
+            # menu = BudgetPage.dropdown["menu"]
+            # menu.delete(0, "end")
+            # for string in BudgetPage.testdd:
+            #     menu.add_command(label=string,
+            #                      command=lambda value=string: BudgetPage.ddvar.set(value))
+            # BudgetPage.update_option_menu(BudgetPage)
+
+            top.destroy()
 
     def plot_data(self, canvas):
         # Clear axes
@@ -480,15 +399,15 @@ class BudgetPage(tk.Frame):
         end_year = self.ey_var.get()
         end_str = end_month + " " + end_year
         end_date = datetime.datetime.strptime(end_str, '%B %Y')
-        # tod = datetime.datetime.today()  # today
-        # duration = (tod - datetime.timedelta(days=(6 * 31))).strftime("%Y-%m")  # six months
 
         # Get filter settings
-        accounts_on = [key for (key, value) in Budgetapp.account_dict.items() if value.get() == 1]
-        categories_on = [key for (key, value) in Budgetapp.category_dict.items() if value.get() == 1]
+        account_dict = self.controller.account_dict
+        category_dict = self.controller.category_dict
+        accounts_on = [key for (key, value) in account_dict.items() if value.get() == 1]
+        categories_on = [key.lower() for (key, value) in category_dict.items() if value.get() == 1]
 
         # Setup Dataframe
-        temp_df = Budgetapp.dataframe.copy()
+        temp_df = self.controller.dataframe.copy()
         # Apply Filters
         temp_df = temp_df[(temp_df['Account Name'].str.contains("|".join(accounts_on))) &
                           (temp_df['Category2'].str.contains("|".join(categories_on)))]
@@ -527,7 +446,7 @@ class BudgetPage(tk.Frame):
     def budget_cal(self):
         tod = datetime.datetime.today()  # today
         duration = (tod - datetime.timedelta(days=(6 * 31))).strftime("%Y-%m")
-        df = Budgetapp.dataframe.copy()
+        df = self.controller.dataframe.copy()
         df = df[df.index > duration]
 
         # Caclulate averages
@@ -542,42 +461,17 @@ class BudgetPage(tk.Frame):
             b *= 1.03  # add 3% margin
             averages.append(b)
 
-        # bills = df[(df['Category2'] == "bills")].groupby(
-        #     pd.Grouper(freq='M'))['Amount'].sum().mean()
-        # bills *= 1.03 # add 3% margin
-        # food = df[(df['Category2'] == "food")].groupby(
-        #     pd.Grouper(freq='M'))['Amount'].sum().mean()
-        # food *= 1.03 # add 3% margin
-        # travel = df[(df['Category2'] == "travel")].groupby(
-        #     pd.Grouper(freq='M'))['Amount'].sum().mean()
-        # travel *= 1.03 # add 3% margin
-        # shopping = df[(df['Category2'] == "shopping")].groupby(
-        #     pd.Grouper(freq='M'))['Amount'].sum().mean()
-        # shopping *= 1.03 # add 3% margin
-        # fun = df[(df['Category2'] == "fun")].groupby(
-        #     pd.Grouper(freq='M'))['Amount'].sum().mean()
-        # fun *= 1.03 # add 3% margin
-        # misc = df[(df['Category2'] == "misc")].groupby(
-        #     pd.Grouper(freq='M'))['Amount'].sum().mean()
-        # misc *= 1.03 # add 3% margin
-        # other = df[(df['Category2'] == "other")].groupby(
-        #     pd.Grouper(freq='M'))['Amount'].sum().mean()
-        # other *= 1.03  # add 3% margin
         savings = income - sum(averages)
 
         budget_list = [income] + averages + [savings]
 
         r = 0
-        for key in Budgetapp.budget_dict.keys():
-            Budgetapp.budget_dict[key].set(value=round(budget_list[r],ndigits=2))
+        for key in self.controller.budget_dict.keys():
+            self.controller.budget_dict[key].set(value=round(budget_list[r], ndigits=2))
             r += 1
 
 
 
 
-
-
-
 app = Budgetapp()
-# ani = animation.FuncAnimation(f,animate, interval=1000)
 app.mainloop()
